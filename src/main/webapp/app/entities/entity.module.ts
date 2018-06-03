d99c370623084a006892b0f8139a2e1b2d9952a5
Gorkem Ercan <gorkem.ercan@gmail.com>
2018-06-03 19:56:16 -0400

    added blog

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { CheDemoBlogModule } from './blog/blog.module';
+import { CheDemoEntryModule } from './entry/entry.module';
+import { CheDemoTagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        CheDemoBlogModule,
+        CheDemoEntryModule,
+        CheDemoTagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
